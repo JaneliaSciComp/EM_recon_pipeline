50aa1fd838787ab57571be2df185d6c342b87bd7
Eric Trautman <trautmane@janelia.hhmi.org>
2021-06-24 09:01:49 -0400

    add dat_to_render.py along with dask-janelia and render-python dependencies

--- create_environment.sh
+++ create_environment.sh
@@ -10,12 +10,15 @@ conda env create -f janelia_emrp.environment.yml
 source "${CONDA_PREFIX}/etc/profile.d/conda.sh"
 conda activate janelia_emrp
 
-# install poetry
-# curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-pip install poetry
+# install poetry (do not use pip install poetry because poetry can break later as other packages are installed)
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 
 # configure poetry (sets up pyproject.toml)
-# poetry init
+poetry init
 
 #poetry add git+https://github.com/janelia-cosem/fibsem-tools.git
 poetry add fibsem_tools
+poetry add dask-janelia
+
+# render-python 2.3.0 has not been published to PyPi yet so need to pull from GitHub
+poetry add git+https://github.com/AllenInstitute/render-python.git
