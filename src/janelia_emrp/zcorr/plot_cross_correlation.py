f770cc1807e4efcf57cbc1be880e2aeb98fa960b
Eric Trautman <trautmane@janelia.hhmi.org>
2021-08-06 11:01:09 -0400

    add support for navigating to CATMAID when clicking correlation values in plot

--- src/janelia_emrp/zcorr/plot_cross_correlation.py
+++ src/janelia_emrp/zcorr/plot_cross_correlation.py
@@ -6,7 +6,7 @@ import sys
 
 import requests
 from bokeh.io import output_file
-from bokeh.models import Range1d
+from bokeh.models import Range1d, ColumnDataSource, TapTool, OpenURL
 from bokeh.plotting import figure, show
 
 
@@ -23,8 +23,23 @@ def load_cross_correlation_file_data(cc_data_path):
     return json.loads(json_str)
 
 
-def plot_correlations_with_next(title, cc_data_path, plot_width=2400, plot_height=400,
-                                output_file_path=None, first_z=1):
+def get_stack_metadata(owner, project, stack):
+    host = 'tem-services.int.janelia.org:8080'
+    url = f'http://{host}/render-ws/v1/owner/{owner}/project/{project}/stack/{stack}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        stack_metadata = response.json()
+    else:
+        raise Exception(f'status code {response.status_code} returned for {url}')
+
+    return stack_metadata
+
+
+def plot_correlations_with_next(title, cc_data_path, owner, project, stack,
+                                plot_width=2400, plot_height=400, output_file_path=None):
+    stack_metadata = get_stack_metadata(owner, project, stack)
+    bounds = stack_metadata["stats"]["stackBounds"]
+    stack_version = stack_metadata["currentVersion"]
 
     # "layerCount" : 27499,
     # "comparisonRange" : 10,
@@ -32,15 +47,18 @@ def plot_correlations_with_next(title, cc_data_path, plot_width=2400, plot_heigh
     # "data" : [][]
     merged_cc_result = load_cross_correlation_file_data(cc_data_path)
 
-    z_offset = first_z + merged_cc_result["firstLayerOffset"]
+    z_offset = bounds["minZ"] + merged_cc_result["firstLayerOffset"]
     data = merged_cc_result["data"]
 
     z_values = []
+    zp_values = []
     cc_with_next = []
     min_cc = 1.0
     max_cc = 0.0
     for i in range(0, len(data) - 1):
-        z_values.append(z_offset + i)
+        z = z_offset + i
+        z_values.append(z)
+        zp_values.append(z * stack_version["stackResolutionZ"])
         cc = data[i][0]
 
         if not isinstance(cc, float):
@@ -57,32 +75,28 @@ def plot_correlations_with_next(title, cc_data_path, plot_width=2400, plot_heigh
         output_file(output_file_path)
         print(f'writing plot to {output_file_path}')
 
-    tooltips = [("z", "@x"), ("correlation with next", "@y")]
+    tooltips = [("z", "@x"), ("correlation with next", "@y"), ("to view in CATMAID", "click point")]
     p = figure(title=title, x_axis_label='z', y_axis_label='correlation with next layer',
-               tooltips=tooltips, plot_width=plot_width, plot_height=plot_height,
+               tooltips=tooltips, tools='tap',
+               plot_width=plot_width, plot_height=plot_height,
                y_range=Range1d(min_cc, max_cc))
 
-    p.circle(z_values, cc_with_next)
+    data_source = ColumnDataSource(data=dict(x=z_values, y=cc_with_next, zp=zp_values))
+    p.circle(source=data_source)
+
+    # open CATMAID when circle glyphs are clicked
+    xp = int( bounds["minX"] + (bounds["maxX"] - bounds["minX"]) / 2) * stack_version["stackResolutionX"]
+    yp = int( bounds["minY"] + (bounds["maxY"] - bounds["minY"]) / 2) * stack_version["stackResolutionY"]
+    catmaid_base_url = 'http://renderer-catmaid.int.janelia.org:8000'
+    catmaid_url = f'{catmaid_base_url}/?pid={owner}__{project}&sid0={stack}&tool=navigator&s0=5&xp={xp}&yp={yp}&zp=@zp'
+
+    tap_tool = p.select(type=TapTool)
+    tap_tool.callback = OpenURL(url=catmaid_url)
 
     show(p)
 
 
-def get_stack_first_z(owner, project, stack):
-    first_z = 1
-    host = 'tem-services.int.janelia.org:8080'
-    url = f'http://{host}/render-ws/v1/owner/{owner}/project/{project}/stack/{stack}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        stack_metadata = response.json()
-        first_z = stack_metadata["stats"]["stackBounds"]["minZ"]
-    else:
-        print(f'WARNING: status code {response.status_code} returned for {url}, assuming first z is 1')
-
-    return first_z
-
-
 def plot_run(owner, project, stack, run):
-    first_z = get_stack_first_z(owner, project, stack)
     owner_run_sub_path = f'{owner}/{project}/{stack}/{run}'
     run_dir = f'/nrs/flyem/render/z_corr/{owner_run_sub_path}'
     plot_html_name = 'cc_with_next_plot.html'
@@ -91,8 +105,8 @@ def plot_run(owner, project, stack, run):
 
     plot_correlations_with_next(title=f'{owner} : {project} : {stack} correlations with next',
                                 cc_data_path=f'{run_dir}/merged_cc_data.json.gz',
-                                output_file_path=output_file_path,
-                                first_z=first_z)
+                                owner=owner, project=project, stack=stack,
+                                output_file_path=output_file_path)
     print(f'view plot at {plot_url}')
 
 
