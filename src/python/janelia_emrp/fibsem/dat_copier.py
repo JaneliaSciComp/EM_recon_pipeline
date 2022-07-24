09a434e7a938d691627bea3ca5b89ca326f5d640
Eric Trautman <trautmane@janelia.hhmi.org>
2022-07-24 11:10:24 -0400

    handle partial first layers when checking for missing dat files

--- src/python/janelia_emrp/fibsem/dat_copier.py
+++ src/python/janelia_emrp/fibsem/dat_copier.py
@@ -151,10 +151,12 @@ def find_missing_scope_dats_for_day(scope_dat_paths: list[Path],
                                     cluster_root_dat_path: Path,
                                     start_time: datetime.datetime,
                                     stop_time: datetime.datetime,
+                                    first_keep_file: KeepFile,
                                     time_to_keep_files: Dict[datetime.datetime, list[KeepFile]]):
 
     missing_scope_dats: list[Path] = []
 
+    first_keep_file_name = Path(first_keep_file.dat_path).name
     keep_layer_times = time_to_keep_files.keys()
 
     for scope_dat in scope_dat_paths:
@@ -169,6 +171,10 @@ def find_missing_scope_dats_for_day(scope_dat_paths: list[Path],
                     if keep_file.dat_path.casefold() == str(scope_dat).casefold():
                         is_missing = False
                         break
+                # check dats in same layer as first keep file
+                if is_missing and dat_path.file_path.name < first_keep_file_name:
+                    is_missing = not dat_path.file_path.exists()
+
             else:
                 is_missing = not dat_path.file_path.exists()
         else:
@@ -209,6 +215,7 @@ def find_missing_scope_dats(keep_file_list: list[KeepFile],
                                             cluster_root_dat_path=cluster_root_dat_path,
                                             start_time=nothing_missing_before,
                                             stop_time=last_keep_time,
+                                            first_keep_file=keep_file_list[0],
                                             time_to_keep_files=time_to_keep_files))
     return missing_scope_dats
 
