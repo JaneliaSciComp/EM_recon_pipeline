ee65d6af89819a2a9d89ceb1c857d332f4cd725f
Eric Trautman <trautmane@janelia.hhmi.org>
2022-09-05 18:43:32 -0400

    improve convert dats log organization

--- src/scripts/flyem/dat_transfer/02_convert_dats.sh
+++ src/scripts/flyem/dat_transfer/02_convert_dats.sh
@@ -4,26 +4,27 @@ set -e
 
 umask 0002
 
-ABSOLUTE_SCRIPT=`readlink -m $0`
-SCRIPT_DIR=`dirname ${ABSOLUTE_SCRIPT}`
-
-if (( $# < 2 )); then
-  echo "USAGE $0 <transfer info file> <num workers> [first_dat] [last_dat]"
+if (( $# < 3 )); then
+  echo "USAGE $0 <transfer info file> <num workers> <base logs dir> [first_dat] [last_dat]"
   exit 1
 fi
 
 TRANSFER_INFO="$1"
 NUM_WORKERS="$2"
+BASE_LOGS_DIR="$3"
 
 if [[ "${HOSTNAME}" =~ ^(e05u15|e05u16) ]]; then
   echo "ERROR: running on login1 or login2, need to bsub this job first ..."
   exit 1
 fi
 
-RUN_TIME=$(date +"%Y%m%d_%H%M%S")
-RUN_MONTH=`date +"%Y%m"`
-LOG_DIR="${SCRIPT_DIR}/logs/convert_dat/${RUN_MONTH}/${RUN_TIME}"
-mkdir -p ${LOG_DIR}
+RUN_DATE_AND_TIME=$(date +"%Y%m%d_%H%M%S")
+RUN_YEAR_MONTH=$(echo "${RUN_DATE_AND_TIME}" | cut -c1-6)
+RUN_DAY=$(echo "${RUN_DATE_AND_TIME}" | cut -c7-8)
+RUN_TIME=$(echo "${RUN_DATE_AND_TIME}" | cut -c10-)
+LOG_DIR="${BASE_LOGS_DIR}/convert/${RUN_YEAR_MONTH}/${RUN_DAY}/${RUN_TIME}"
+mkdir -p "${LOG_DIR}"
+
 LOG_FILE="${LOG_DIR}/convert_dat.log"
 
 source /groups/flyem/data/render/bin/miniconda3/source_me.sh
@@ -38,7 +39,6 @@ EMRP_ROOT="/groups/flyem/data/render/git/EM_recon_pipeline"
 export PYTHONPATH="${EMRP_ROOT}/src/python"
 
 ARGS="${EMRP_ROOT}/src/python/janelia_emrp/fibsem/dat_converter.py"
-#ARGS="${ARGS} --volume_transfer_dir ${SCRIPT_DIR}/config"
 ARGS="${ARGS} --volume_transfer_info ${TRANSFER_INFO}"
 ARGS="${ARGS} --num_workers ${NUM_WORKERS}"
 ARGS="${ARGS} --parent_work_dir ${LOG_DIR}"
@@ -54,11 +54,11 @@ fi
 ARGS="${ARGS} --lsf_runtime_limit 3:59"
 
 echo """
-On ${HOSTNAME} at ${RUN_TIME}
+On ${HOSTNAME} at ${RUN_DATE_AND_TIME}
 
 Running:
   python ${ARGS}
-""" | tee -a ${LOG_FILE}
+""" | tee -a "${LOG_FILE}"
 
 # The exit status of a pipeline is the exit status of the last command in the pipeline,
 # unless the pipefail option is enabled (see The Set Builtin).
@@ -66,7 +66,7 @@ Running:
 # to exit with a non-zero status, or zero if all commands exit successfully.
 set -o pipefail
 
-python ${ARGS} 2>&1 | tee -a ${LOG_FILE}
+python ${ARGS} 2>&1 | tee -a "${LOG_FILE}"
 RETURN_CODE="$?"
 
 echo "python return code is ${RETURN_CODE}"
