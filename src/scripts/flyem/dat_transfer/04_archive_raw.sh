d4a311e77708c05b07c8f971c4a2cfaf3c04481e
Eric Trautman <trautmane@janelia.hhmi.org>
2022-09-06 10:47:15 -0400

    add archive raw wrapper script

---
+++ src/scripts/flyem/dat_transfer/04_archive_raw.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+umask 0002
+
+ABSOLUTE_SCRIPT=$(readlink -m "$0")
+SCRIPT_DIR=$(dirname "${ABSOLUTE_SCRIPT}")
+
+if (( $# < 1 )); then
+  echo "USAGE $0 <num rsync processes>"
+  exit 1
+fi
+
+NUM_PROCESSES="$1"
+
+RUN_DATE_AND_TIME=$(date +"%Y%m%d_%H%M%S")
+RUN_YEAR_MONTH=$(echo "${RUN_DATE_AND_TIME}" | cut -c1-6)
+RUN_DAY=$(echo "${RUN_DATE_AND_TIME}" | cut -c7-8)
+LOG_DIR="${SCRIPT_DIR}/logs/archive_raw/${RUN_YEAR_MONTH}/${RUN_DAY}"
+mkdir -p "${LOG_DIR}"
+
+LOG_FILE="${LOG_DIR}/archive_raw.${RUN_DATE_AND_TIME}.log"
+
+source /groups/flyem/data/render/bin/miniconda3/source_me.sh
+
+conda activate janelia_emrp
+
+EMRP_ROOT="/groups/flyem/data/render/git/EM_recon_pipeline"
+
+export PYTHONPATH="${EMRP_ROOT}/src/python"
+
+ARGS="${EMRP_ROOT}/src/python/janelia_emrp/fibsem/h5_archivist.py"
+ARGS="${ARGS} --volume_transfer_dir ${SCRIPT_DIR}/config"
+ARGS="${ARGS} --processes ${NUM_PROCESSES}"
+
+echo """
+On ${HOSTNAME} at ${RUN_DATE_AND_TIME}
+
+Running:
+  python ${ARGS}
+""" | tee -a "${LOG_FILE}"
+
+# The exit status of a pipeline is the exit status of the last command in the pipeline,
+# unless the pipefail option is enabled (see The Set Builtin).
+# If pipefail is enabled, the pipeline's return status is the value of the last (rightmost) command
+# to exit with a non-zero status, or zero if all commands exit successfully.
+set -o pipefail
+
+python ${ARGS} 2>&1 | tee -a "${LOG_FILE}"
+RETURN_CODE="$?"
+
+echo "python return code is ${RETURN_CODE}"
+exit ${RETURN_CODE}
