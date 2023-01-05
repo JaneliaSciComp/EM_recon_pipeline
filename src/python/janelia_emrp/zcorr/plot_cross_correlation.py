500b4514280e659b126067cdd286ec78f1f249cf
Eric Trautman <trautmane@janelia.hhmi.org>
2023-01-05 12:06:28 -0500

    add support for neuroglancer links to cross correlation plots and make them the default (instead of CATMAID)

--- src/python/janelia_emrp/zcorr/plot_cross_correlation.py
+++ src/python/janelia_emrp/zcorr/plot_cross_correlation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import argparse
+import json
+import urllib.parse
 
 import sys
 
@@ -11,10 +13,13 @@ from janelia_emrp.zcorr.plot_util import get_stack_metadata, load_json_file_data
 
 
 def plot_correlations_with_next(title, cc_data_path, owner, project, stack,
-                                plot_width=2400, plot_height=400, output_file_path=None):
+                                tap_to_ng=True, plot_width=2400, plot_height=400, output_file_path=None):
     stack_metadata = get_stack_metadata(owner, project, stack)
     bounds = stack_metadata["stats"]["stackBounds"]
     stack_version = stack_metadata["currentVersion"]
+    res_x = stack_version["stackResolutionX"]
+    res_y = stack_version["stackResolutionY"]
+    res_z = stack_version["stackResolutionZ"]
 
     # "layerCount" : 27499,
     # "comparisonRange" : 10,
@@ -33,7 +38,7 @@ def plot_correlations_with_next(title, cc_data_path, owner, project, stack,
     for i in range(0, len(data) - 1):
         z = z_offset + i
         z_values.append(z)
-        zp_values.append(z * stack_version["stackResolutionZ"])
+        zp_values.append(z * res_z)
         cc = data[i][0]
 
         if not isinstance(cc, float):
@@ -59,16 +64,65 @@ def plot_correlations_with_next(title, cc_data_path, owner, project, stack,
     data_source = ColumnDataSource(data=dict(x=z_values, y=cc_with_next, zp=zp_values))
     p.circle(source=data_source)
 
-    # open CATMAID when circle glyphs are clicked
-    xp = int(bounds["minX"] + (bounds["maxX"] - bounds["minX"]) / 2) * stack_version["stackResolutionX"]
-    yp = int(bounds["minY"] + (bounds["maxY"] - bounds["minY"]) / 2) * stack_version["stackResolutionY"]
-    catmaid_base_url = 'http://renderer-catmaid.int.janelia.org:8000'
-    catmaid_url = f'{catmaid_base_url}/?pid={owner}__{project}&sid0={stack}&tool=navigator&s0=5&xp={xp}&yp={yp}&zp=@zp'
+    # open Neuroglancer or CATMAID when circle glyphs are clicked
+    center_x = int(bounds["minX"] + (bounds["maxX"] - bounds["minX"]) / 2)
+    center_y = int(bounds["minY"] + (bounds["maxY"] - bounds["minY"]) / 2)
+
+    if tap_to_ng:
+        tap_url = build_neuroglancer_tap_url(owner, project, stack, res_x, res_y, res_z, center_x, center_y,
+                                             z_token="@z")
+    else:
+        catmaid_base_url = 'http://renderer-catmaid.int.janelia.org:8000'
+        xp = center_x * res_x
+        yp = center_y * res_y
+        tap_url = f'{catmaid_base_url}/?pid={owner}__{project}&sid0={stack}&tool=navigator&s0=5&xp={xp}&yp={yp}&zp=@zp'
 
     tap_tool = p.select(type=TapTool)
-    tap_tool.callback = OpenURL(url=catmaid_url)
+    tap_tool.callback = OpenURL(url=tap_url)
 
     show(p)
+
+
+def build_neuroglancer_tap_url(owner: str,
+                               project: str,
+                               stack: str,
+                               res_x: int,
+                               res_y: int,
+                               res_z: int,
+                               center_x: int,
+                               center_y: int,
+                               z_token: str) -> str:
+
+    ng_source_url = f'render://http://renderer.int.janelia.org:8080/{owner}/{project}/{stack}'
+    layer_name = f'{project} {stack}'
+    replace_with_z_token_after_encoding = 0.123456789
+    ng_state = {
+        "dimensions": {
+            "x": [res_x, "nm"],
+            "y": [res_y, "nm"],
+            "z": [res_z, "nm"]
+        },
+        "position": [center_x, center_y, replace_with_z_token_after_encoding],
+        "crossSectionScale": 32, "projectionScale": 32768,
+        "layers": [
+            {
+                "type": "image",
+                "source": {
+                    "url": ng_source_url,
+                    "subsources": {"default": True, "bounds": True}, "enableDefaultSubsources": False
+                },
+                "tab": "source",
+                "name": layer_name
+            }
+        ],
+        "selectedLayer": {"layer": layer_name},
+        "layout": "xy"
+    }
+    ng_state_json_string = json.dumps(ng_state)
+    encoded_ng_state = urllib.parse.quote(ng_state_json_string)
+    encoded_ng_state_with_at_z = encoded_ng_state.replace(str(replace_with_z_token_after_encoding), z_token)
+    ng_url = f'http://renderer.int.janelia.org:8080/ng/#!{encoded_ng_state_with_at_z}'
+    return ng_url
 
 
 def plot_run(base_dir, owner, project, stack, run):
