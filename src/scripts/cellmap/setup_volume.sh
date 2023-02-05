c65bca5fbfd9d7ff09965105f34b36164f24811d
Eric Trautman <trautmane@janelia.hhmi.org>
2023-02-04 20:46:44 -0500

    refactor setup_volume script to use volume transfer info

--- src/scripts/cellmap/setup_volume.sh
+++ src/scripts/cellmap/setup_volume.sh
@@ -6,25 +6,70 @@ ABSOLUTE_SCRIPT=$(readlink -m "$0")
 SCRIPT_DIR=$(dirname "${ABSOLUTE_SCRIPT}")
 BASE_SCRIPTS_DIR="${SCRIPT_DIR}/base_scripts"
 
-if (( $# < 2 )); then
-  echo "
-USAGE: $0 <volume name> <single_row | multi_row> [lab | project_group]
+EMRP_ROOT="/groups/flyem/data/render/git/EM_recon_pipeline"
+TRANSFER_INFO_DIR="${EMRP_ROOT}/src/resources/transfer_info"
 
-For Example:
-       $0 jrc_mus-lung-covid single_row
-       $0 Z0422_17_VNC_1 multi_row fibsem
+RUN_DIR=$(readlink -m ".")
+cd "${TRANSFER_INFO_DIR}"
+
+unset TRANSFER_INFO_JSON_FILE
+shopt -s nullglob
+TI_FILES=(*/volume_transfer_info.*.json)
+shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
+
+echo "
+Version controlled volume_transfer_info files are:
 "
+PS3="
+Choose number of volume_transfer_info file for new data set: "
+select TI_FILE in "${TI_FILES[@]}"; do
+  break
+done
+
+TRANSFER_INFO_JSON_FILE="${TRANSFER_INFO_DIR}/${TI_FILE}"
+TI_BASENAME=$(basename "${TI_FILE}")
+
+cd "${RUN_DIR}"
+
+if [[ ! -f "${TRANSFER_INFO_JSON_FILE}" ]]; then
+  echo "ERROR: ${TRANSFER_INFO_JSON_FILE} not found!"
   exit 1
 fi
 
-VOLUME_NAME="$1"
-LAYOUT="$2"
-LAB_OR_PROJECT_GROUP="${3:-cellmap}"
+VOLUME_NAME="${TI_BASENAME#volume_transfer_info.}"
+VOLUME_NAME="${VOLUME_NAME%.json}"
+
+JQ="/groups/flyem/data/render/bin/jq"
+
+LAB_OR_PROJECT_GROUP=$(${JQ} '.render_data_set.owner' "${TRANSFER_INFO_JSON_FILE}" | sed 's/"//g')
+
+ROW_COUNT=$(${JQ} '.scope_data_set.rows_per_z_layer' "${TRANSFER_INFO_JSON_FILE}")
+NULL_VALUE="null"
+
+if [[ "${ROW_COUNT}" -eq "${NULL_VALUE}" || "${ROW_COUNT}" -eq "0" || "${ROW_COUNT}" -eq "1" ]]; then
+  LAYOUT="single_row"
+else
+  LAYOUT="multi_row"
+fi
 
 ALIGN_DIR="/groups/${LAB_OR_PROJECT_GROUP}/${LAB_OR_PROJECT_GROUP}/render/align/${VOLUME_NAME}"
 if [[ -d "${ALIGN_DIR}" ]]; then
   echo "ERROR: ${ALIGN_DIR} already exists!"
 fi
+
+echo "
+After parsing
+  ${TRANSFER_INFO_JSON_FILE}
+
+alignment scripts will be setup with
+  owner:       ${LAB_OR_PROJECT_GROUP}
+  volume name: ${VOLUME_NAME}
+  layout:      ${LAYOUT}
+
+in directory:
+    ${ALIGN_DIR}
+"
+
 mkdir -p "${ALIGN_DIR}"
 chmod 2775 "${ALIGN_DIR}"
 
@@ -36,4 +81,4 @@ sed -i "
   s/export LAYOUT.*/export LAYOUT=\"${LAYOUT}\"/
 " "${ALIGN_DIR}"/00_config.sh
 
-echo "created ${ALIGN_DIR}"
+ls "${ALIGN_DIR}"
