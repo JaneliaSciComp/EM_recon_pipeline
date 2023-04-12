f168ec049c3bec22e53e78b94cf8021b467958ff
Eric Trautman <trautmane@janelia.hhmi.org>
2023-04-12 13:48:53 -0400

    add RenderRequest helper class

--- src/python/janelia_emrp/render/web_service_request.py
+++ src/python/janelia_emrp/render/web_service_request.py
@@ -14,12 +14,45 @@ def submit_get(url: str,
 
 
 def submit_put(url: str,
-               json: Union[dict[str, Any], list[dict[str, Any]]],
+               json: Optional[Union[dict[str, Any], list[dict[str, Any]]]],
                context: Optional[str] = None) -> None:
     extra_context = "" if context is None else f" {context}"
     print(f"submitting PUT {url}{extra_context}")
     put_response = requests.put(url, json=json)
     put_response.raise_for_status()
+
+
+@dataclass
+class RenderRequest:
+    host: str
+    owner: str
+    project: str
+
+    def stack_url(self, stack) -> str:
+        # noinspection HttpUrlsUsage
+        return f"http://{self.host}/render-ws/v1/owner/{self.owner}/project/{self.project}/stack/{stack}"
+
+    def get_tile_spec(self, stack, tile_id) -> dict[str, Any]:
+        return submit_get(f'{self.stack_url(stack)}/tile/{tile_id}')
+
+    def get_resolved_tiles_for_layer(self, stack, z):
+        return submit_get(f'{self.stack_url(stack)}/z/{z}/resolvedTiles')
+
+    def set_stack_state(self, stack, state):
+        url = f'{self.stack_url(stack)}/state/{state}'
+        submit_put(url=url, json=None, context=None)
+
+    def set_stack_state_to_loading(self, stack):
+        self.set_stack_state(stack, 'LOADING')
+
+    def set_stack_state_to_complete(self, stack):
+        self.set_stack_state(stack, 'COMPLETE')
+
+    def save_resolved_tiles(self, stack, resolved_tiles):
+        url = f'{self.stack_url(stack)}/resolvedTiles'
+        submit_put(url=url,
+                   json=resolved_tiles,
+                   context=f'for {len(resolved_tiles["tileIdToSpecMap"])} tile specs')
 
 
 @dataclass
