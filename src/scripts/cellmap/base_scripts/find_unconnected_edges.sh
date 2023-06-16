6a589131464760414f45ee3272bb63a8102336fa
Eric Trautman <trautmane@janelia.hhmi.org>
2023-06-16 09:02:36 -0400

    show no unconnected edges found message

--- src/scripts/cellmap/base_scripts/find_unconnected_edges.sh
+++ src/scripts/cellmap/base_scripts/find_unconnected_edges.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-ABSOLUTE_SCRIPT=`readlink -m $0`
-SCRIPT_DIR=`dirname ${ABSOLUTE_SCRIPT}`
-source ${SCRIPT_DIR}/00_config.sh
+ABSOLUTE_SCRIPT=$(readlink -m "$0")
+SCRIPT_DIR=$(dirname "${ABSOLUTE_SCRIPT}")
+source "${SCRIPT_DIR}/00_config.sh"
 
 if (( $# != 1 )); then
   echo "USAGE: $0 <stack>"
@@ -16,7 +16,7 @@ STACK="$1"
 LOG_DIR="${SCRIPT_DIR}/logs"
 LOG="${LOG_DIR}/unconnected_edges.${STACK}.$(date +"%Y%m%d_%H%M%S").log"
 
-mkdir -p ${LOG_DIR}
+mkdir -p "${LOG_DIR}"
 
 ARGS="org.janelia.render.client.UnconnectedTileEdgesClient"
 ARGS="${ARGS} --baseDataUrl http://${SERVICE_HOST}/render-ws/v1"
@@ -31,6 +31,7 @@ Unconnected edge data will be written to:
 Be patient, this could take a few minutes ...
 "
 
-${RENDER_CLIENT_SCRIPT} 1G ${ARGS} > ${LOG}
+# shellcheck disable=SC2086
+"${RENDER_CLIENT_SCRIPT}" 1G ${ARGS} > "${LOG}"
 
-grep "findUnconnectedEdges: edge" ${LOG} | sed 's/.*findUnconnectedEdges: //'
+grep "findUnconnectedEdges: " "${LOG}" | sed 's/.*findUnconnectedEdges: //'
