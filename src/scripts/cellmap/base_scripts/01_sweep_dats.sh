16d045c985dd81a6feaaa660aa345487aca483f3
Eric Trautman <trautmane@janelia.hhmi.org>
2023-09-01 08:38:30 -0400

    update sweep script to use dask

--- src/scripts/cellmap/base_scripts/01_sweep_dats.sh
+++ src/scripts/cellmap/base_scripts/01_sweep_dats.sh
@@ -16,11 +16,11 @@ fi
 JQ="/groups/flyem/data/render/bin/jq"
 SCOPE=$(${JQ} '.scope_data_set.host' "${TRANSFER_INFO_JSON}")
 
-LOG_DIR="${SCRIPT_DIR}/logs"
-mkdir -p "${LOG_DIR}"
-LOG_FILE="${LOG_DIR}/sweep_dat.log"
+SWEEP_LOG_DIR="${SCRIPT_DIR}/logs/sweep"
+mkdir -p "${SWEEP_LOG_DIR}"
+LOG_FILE="${SWEEP_LOG_DIR}/sweep_dat.log"
 
-source /groups/flyem/data/render/bin/miniconda3/source_me.sh 
+source /groups/flyem/data/render/bin/miniconda3/source_me.sh
 
 conda activate janelia_emrp
 
@@ -31,7 +31,8 @@ export PYTHONPATH="${EMRP_ROOT}/src/python"
 ARGS="${EMRP_ROOT}/src/python/janelia_emrp/fibsem/dat_sweeper.py"
 ARGS="${ARGS} --volume_transfer_dir ${SCRIPT_DIR}"
 ARGS="${ARGS} --scope ${SCOPE}"
-ARGS="${ARGS} --max_transfer_minutes 240 $*"
+ARGS="${ARGS} --max_transfer_minutes 240"
+ARGS="${ARGS} --num_workers 8 --parent_work_dir ${SWEEP_LOG_DIR} $*"
 
 echo """
 On ${HOSTNAME} at ${RUN_TIME}
@@ -40,4 +41,4 @@ Running (as flyem user for connection to scope):
   python ${ARGS}
 """ | tee -a "${LOG_FILE}"
 
-su -c "python ${ARGS} 2>&1 | tee -a ${LOG_FILE}" flyem
+su -c "umask 0002; python ${ARGS} 2>&1 | tee -a ${LOG_FILE}" flyem
