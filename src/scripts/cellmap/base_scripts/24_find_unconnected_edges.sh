e8ba4fd4abf36dc9938da17fb004a3031e8b753f
Eric Trautman <trautmane@janelia.hhmi.org>
2024-03-09 18:30:56 -0500

    merge count cluster and find unconnected edge tools

--- src/scripts/cellmap/base_scripts/24_find_unconnected_edges.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ABSOLUTE_SCRIPT=$(readlink -m "${0}")
-SCRIPT_DIR=$(dirname "${ABSOLUTE_SCRIPT}")
-source "${SCRIPT_DIR}"/00_config.sh
-
-"${SCRIPT_DIR}"/find_unconnected_edges.sh "${ACQUIRE_TRIMMED_STACK}"
