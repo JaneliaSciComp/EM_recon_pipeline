544c14b4311787243313a28e007a19685842da58
Eric Trautman <trautmane@janelia.hhmi.org>
2024-10-02 08:40:47 -0400

    move rarely used spec run validation and cluster count scripts to old subdirectory

--- src/scripts/cellmap/base_scripts/23_count_trimmed_clusters.sh
+++ src/scripts/cellmap/base_scripts/23_count_trimmed_clusters.sh
@@ -6,8 +6,23 @@ ABSOLUTE_SCRIPT=$(readlink -m "${0}")
 SCRIPT_DIR=$(dirname "${ABSOLUTE_SCRIPT}")
 source "${SCRIPT_DIR}"/00_config.sh
 
-# make sure trimmed stack is complete
-COMPLETE_URL="http://${SERVICE_HOST}/render-ws/v1/owner/${RENDER_OWNER}/project/${RENDER_PROJECT}/stack/${ACQUIRE_TRIMMED_STACK}/state/COMPLETE"
-curl -v -X PUT "${COMPLETE_URL}"
+LOG_DIR="${SCRIPT_DIR}/logs"
+COUNT_LOG="${LOG_DIR}/cluster_count.${ACQUIRE_TRIMMED_STACK}.log"
 
-"${SCRIPT_DIR}"/count_clusters.sh "${ACQUIRE_TRIMMED_STACK}"
+mkdir -p "${LOG_DIR}"
+
+ARGS="org.janelia.render.client.ClusterCountClient"
+ARGS="${ARGS} --baseDataUrl http://${SERVICE_HOST}/render-ws/v1"
+ARGS="${ARGS} --owner ${RENDER_OWNER} --project ${RENDER_PROJECT} --stack ${ACQUIRE_TRIMMED_STACK}"
+ARGS="${ARGS} --matchCollection ${MATCH_COLLECTION}"
+ARGS="${ARGS} --maxSmallClusterSize 0 --includeMatchesOutsideGroup --maxLayersPerBatch 1000 --maxOverlapLayers 6"
+ARGS="${ARGS} --maxLayersForUnconnectedEdge 50"
+
+echo "
+Connected tile cluster counts will be written to:
+  ${COUNT_LOG}
+
+Be patient, this could take a few minutes ...
+"
+
+${RENDER_CLIENT_SCRIPT} 1G ${ARGS} > "${COUNT_LOG}"
--- src/scripts/cellmap/base_scripts/patch/23_count_trimmed_clusters.sh
+++ src/scripts/cellmap/base_scripts/patch/23_count_trimmed_clusters.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-set -e
-
-ABSOLUTE_SCRIPT=`readlink -m $0`
-SCRIPT_DIR=`dirname ${ABSOLUTE_SCRIPT}`
-source ${SCRIPT_DIR}/../00_config.sh
-
-${SCRIPT_DIR}/../count_clusters.sh ${ACQUIRE_TRIMMED_STACK}
+../23_count_trimmed_clusters.sh
