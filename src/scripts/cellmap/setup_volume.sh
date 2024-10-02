14eda6a415a4ac5ae2e9f047c19ceb2fcb7f7d30
Eric Trautman <trautmane@janelia.hhmi.org>
2024-10-02 10:08:25 -0400

    copy new support script directory when setting up

--- src/scripts/cellmap/setup_volume.sh
+++ src/scripts/cellmap/setup_volume.sh
@@ -93,6 +93,7 @@ chmod 2775 "${ALIGN_DIR}"
 
 cp "${BASE_SCRIPTS_DIR}"/*.* "${ALIGN_DIR}"
 cp -r "${BASE_SCRIPTS_DIR}"/match_*_row "${ALIGN_DIR}"
+cp -r "${BASE_SCRIPTS_DIR}"/support "${ALIGN_DIR}"
 cp "${TRANSFER_INFO_JSON_FILE}" "${ALIGN_DIR}"
 
 sed -i "
