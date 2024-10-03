84ba62726f2c8ef7a86ee530883c17d4ec254aaf
trautmane <eric@trautman.us>
2024-10-03 10:01:27 -0400

    fi xmissing dat files for jrc_pri-neuro_0710dish-4

--- src/scripts/flyem/dat_transfer/fix_missing_dat_problem.sh
+++ src/scripts/flyem/dat_transfer/fix_missing_dat_problem.sh
@@ -6,35 +6,20 @@ umask 0002
 
 NUM_WORKERS="1"
 
-#TRANSFER_INFO="/groups/flyem/home/flyem/bin/dat_transfer/2022/config/complete/volume_transfer_info.jrc_22ak351-leaf-3m.json"
-#PARENT_WORK_DIR="/groups/stern/sternlab/em_data/jrc_22ak351-leaf-3m/dat/logs/fix"
-#FIRST_DAT="Merlin-6282_22-10-17_040352_0-0-0.dat"
-#LAST_DAT="Merlin-6282_22-10-17_040352_0-0-3.dat"
-
-#TRANSFER_INFO="/groups/flyem/home/flyem/bin/dat_transfer/2022/config/complete/volume_transfer_info.jrc_22ak351-leaf-3r.json"
-#PARENT_WORK_DIR="/groups/stern/sternlab/em_data/jrc_22ak351-leaf-3r/dat/logs/fix"
-#FIRST_DAT="Merlin-4238_22-10-13_062139_0-0-0.dat"
-#LAST_DAT="Merlin-4238_22-10-13_062139_0-0-2.dat"
-
-#TRANSFER_INFO="/groups/flyem/home/flyem/bin/dat_transfer/2022/config/volume_transfer_info.jrc_sepia_MPI-S-DEC_5A.json"
-#PARENT_WORK_DIR="/groups/reiser/reiserlab/data/jrc_sepia_MPI-S-DEC_5A/dat/logs/fix"
-#FIRST_DAT="Merlin-6285_24-06-16_061900_0-0-0.dat"
-#LAST_DAT="Merlin-6285_24-07-03_143414_0-1-2.dat"
-
-#TRANSFER_INFO="/groups/flyem/home/flyem/bin/dat_transfer/2022/config/volume_transfer_info.jrc_sepia_MPI-S-DEC_5A.json"
-#PARENT_WORK_DIR="/groups/reiser/reiserlab/data/jrc_sepia_MPI-S-DEC_5A/dat/logs/fix"
-#FIRST_DAT="Merlin-6285_24-07-04_160342_0-0-0.dat"
-#LAST_DAT="Merlin-6285_24-07-10_113208_0-1-2.dat"
-
 #TRANSFER_INFO="/groups/flyem/home/flyem/bin/dat_transfer/2022/config/volume_transfer_info.jrc_sepia_MPI-S-DEC_5A.json"
 #PARENT_WORK_DIR="/groups/reiser/reiserlab/data/jrc_sepia_MPI-S-DEC_5A/dat/logs/fix"
 #FIRST_DAT="Merlin-6285_24-05-13_161357_0-0-0.dat"
 #LAST_DAT="Merlin-6285_24-07-10_113208_0-1-2.dat"
 
-TRANSFER_INFO="/groups/flyem/home/flyem/bin/dat_transfer/2022/config/complete/volume_transfer_info.jrc_liu-nih_ips-draq5-test-2.json"
-PARENT_WORK_DIR="/groups/fibsem/fibsem/data/jrc_liu-nih_ips-draq5-test-2/dat/logs/fix"
-FIRST_DAT="Merlin-6281_24-08-22_163046_0-0-0.dat"
-LAST_DAT="Merlin-6281_24-08-22_163046_0-0-2.dat"
+#TRANSFER_INFO="/groups/flyem/home/flyem/bin/dat_transfer/2022/config/complete/volume_transfer_info.jrc_liu-nih_ips-draq5-test-2.json"
+#PARENT_WORK_DIR="/groups/fibsem/fibsem/data/jrc_liu-nih_ips-draq5-test-2/dat/logs/fix"
+#FIRST_DAT="Merlin-6281_24-08-22_163046_0-0-0.dat"
+#LAST_DAT="Merlin-6281_24-08-22_163046_0-0-2.dat"
+
+TRANSFER_INFO="/groups/flyem/home/flyem/bin/dat_transfer/2022/config/volume_transfer_info.jrc_pri_neuron_0710Dish4.json"
+PARENT_WORK_DIR="/groups/fibsem/fibsem/data/jrc_pri-neuron_0710Dish4/dat/logs/fix"
+FIRST_DAT="Merlin-6285_24-09-24_093827_0-0-0.dat"
+LAST_DAT="Merlin-6285_24-09-24_094238_0-0-0.dat"
 
 mkdir -p ${PARENT_WORK_DIR}
 
