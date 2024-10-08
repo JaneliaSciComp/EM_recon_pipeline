c04a8e37207273f49e79e3a6d6090f2f9885651f
Eric Trautman <trautmane@janelia.hhmi.org>
2024-10-08 11:11:52 -0400

    add FillInfo class and remove unused find_intensity_range function

--- src/python/janelia_emrp/fibsem/dat_to_scheffer_8_bit_layer.py
+++ src/python/janelia_emrp/fibsem/dat_to_scheffer_8_bit_layer.py
@@ -4,83 +4,58 @@ from pathlib import Path
 import numpy as np
 from PIL import Image
 from fibsem_tools.io import read
+from pydantic import BaseModel
 
 from janelia_emrp.root_logger import init_logger
 
 logger = logging.getLogger(__name__)
 
 
-def find_intensity_range(cyx_dat_record_list: list[np.ndarray],
-                         channel_num: int = 0) -> tuple[float, float]:
+class FillInfo(BaseModel):
+    """Information about a region to fill within one or more tiles in a layer.
+    #
+    # Attributes:
+    #     tile_indexes:
+    #         list of tile indexes to fill
+    #     x:
+    #         upper left x coordinate of the fill region
+    #     y:
+    #         upper left y coordinate of the fill region
+    #     width:
+    #         width of the fill region
+    #     height:
+    #         height of the fill region
+    #     fill_value:
+    #         intensity to apply to all beyond threshold pixels in the fill region
+    #     threshold:
+    #         replace pixels in the region with the fill value when they are greater than this threshold
     """
-    Parameters
-    ----------
-    cyx_dat_record_list: list[np.ndarray]
-        list of two-channel numpy arrays in cyx order from 16-bit .dat files.
+    tile_indexes: list[int]
+    x: int
+    y: int
+    width: int
+    height: int
+    fill_value: int
+    threshold: int
 
-    channel_num: int, default=0
-        index of channel to compress.
+    def fill_region(self,
+                    pixel_array: np.ndarray) -> np.ndarray:
+        writable_pixel_array = pixel_array.copy()
+        filled_count = 0
+        for x in range(self.x, self.x + self.width):
+            for y in range(self.y, self.y + self.height):
+                if writable_pixel_array[y][x] > self.threshold:
+                    writable_pixel_array[y][x] = self.fill_value
+                    filled_count += 1
 
-    Returns
-    -------
-    tuple[float, float]
-        min and max intensities across all tiles in a layer.
-    """
-    low_for_all_tiles = None
-    high_for_all_tiles = None
+        logger.info(f"fill_region: filled {filled_count} pixels with intensities above {self.threshold}")
 
-    saturated = 3  # how close to the boundary do you need to be considered 'saturated'
-    low_bound = -32768.0
-    high_bound = 32767.0
-
-    min_intensity = low_bound + saturated
-    max_intensity = high_bound - saturated
-
-    for index in range(len(cyx_dat_record_list)):
-        cyx_dat_record = cyx_dat_record_list[index]
-
-        logger.info(f"find_intensity_range: processing channel {channel_num} from dat with shape {cyx_dat_record.shape}")
-
-        pixel_array = cyx_dat_record[channel_num, :, :]
-
-        # First, find the mean and standard deviation of the 'real' non-saturated pixels.
-        # Ignore any that are too close to saturated light or dark.
-        unsaturated_pixel_array = pixel_array[np.logical_and(pixel_array >= min_intensity, pixel_array < max_intensity)]
-
-        unsaturated_count = unsaturated_pixel_array.size
-        unsaturated_pct = (unsaturated_count * 100.0) / pixel_array.size
-        logger.info(f"find_intensity_range: {unsaturated_count} real pixels, {unsaturated_pct} percent")
-
-        mean = np.mean(unsaturated_pixel_array).item()
-        std_dev = np.std(unsaturated_pixel_array).item()
-        logger.info(f"find_intensity_range: Of the above image points, mean= {mean} and std dev = {std_dev}")
-
-        # Convert mean-4*sigma -> 0, mean +4 sigma to 255.
-        low = mean - (4 * std_dev)
-        high = mean + (4 * std_dev)
-
-        if low < low_bound:
-            logger.info("find_intensity_range: minus 4 sigma < -32768.  Changing to 0")
-            low = low_bound
-
-        if high > high_bound:
-            logger.info("find_intensity_range: plus 4 sigma > 32767.  Changing to 65535")
-            high = high_bound
-
-        temp = low
-        low = high
-        high = temp  # swap low and high
-
-        logger.info(f"find_intensity_range: low {low:.2f}  -> 0, high {high:.2f} -> 255")
-
-        low_for_all_tiles = low if low_for_all_tiles is None else min(low, low_for_all_tiles)
-        high_for_all_tiles = high if high_for_all_tiles is None else max(high, high_for_all_tiles)
-
-    return (low_for_all_tiles, high_for_all_tiles)
+        return writable_pixel_array
 
 
 def compress_compute_layer(cyx_dat_record_list: list[np.ndarray],
-                           channel_num: int = 0) -> list[np.ndarray]:
+                           channel_num: int = 0,
+                           fill_info: FillInfo = None) -> list[np.ndarray]:
     """
     Adaptation of Lou Scheffer's 16-bit to 8-bit "compression" algorithm from
     /groups/flyem/home/flyem/bin/compress_dats/build2/Compress.cpp
@@ -94,16 +69,23 @@ def compress_compute_layer(cyx_dat_record_list: list[np.ndarray],
     channel_num: int, default=0
         index of channel to compress.
 
+    fill_info: FillInfo, default=None
+        information about a region to fill within one or more tiles in the layer.
+
     Returns
     -------
-    np.ndarray
+    list[np.ndarray]
         list of 8-bit numpy arrays produced by "compression".
     """
     compressed_data_list: list[np.ndarray] = []
     pixel_array_list = []
-    for cyx_dat_record in cyx_dat_record_list:
+    for tile_index in range(len(cyx_dat_record_list)):
+        cyx_dat_record = cyx_dat_record_list[tile_index]
         logger.info(f"compress_compute_layer: processing channel {channel_num} from dat with shape {cyx_dat_record.shape}")
-        pixel_array_list.append(cyx_dat_record[channel_num, :, :])
+        pixel_array = cyx_dat_record[channel_num, :, :]
+        if fill_info is not None and tile_index in fill_info.tile_indexes:
+           pixel_array = fill_info.fill_region(pixel_array)
+        pixel_array_list.append(pixel_array)
 
     stacked_pixel_array = np.stack(pixel_array_list, axis=0)
 
@@ -123,9 +105,6 @@ def compress_compute_layer(cyx_dat_record_list: list[np.ndarray],
     logger.info(f"compress_compute: {unsaturated_count} real pixels, {unsaturated_pct} percent")
 
     mean = np.mean(unsaturated_pixel_array).item()
-
-    # TODO: see if dat header QuantLow and QuantHigh values could be used instead since they seem to be
-    #       the same as the swapped low and high values here (would allow us to skip stdDev calculation)
     std_dev = np.std(unsaturated_pixel_array).item()
 
     logger.info(f"compress_compute: Of the above image points, mean= {mean} and std dev = {std_dev}")
@@ -186,7 +165,8 @@ def compress_compute_layer(cyx_dat_record_list: list[np.ndarray],
 
 
 def compress_and_save_layer(dat_path_list: list[Path],
-                            compressed_path_list: list[Path]):
+                            compressed_path_list: list[Path],
+                            fill_info: FillInfo = None):
     """
     Compresses the specified list of dat files and saves them to the specified paths.
 
@@ -197,6 +177,9 @@ def compress_and_save_layer(dat_path_list: list[Path],
 
     compressed_path_list: Path
         list of compressed output paths.
+
+    fill_info: FillInfo, default=None
+        information about a region to fill within one or more tiles in the layer.
     """
     cyx_dat_record_list: list[np.ndarray] = []
 
@@ -207,7 +190,9 @@ def compress_and_save_layer(dat_path_list: list[Path],
         logger.info(f"compress_and_save: loaded {cyx_dat_record.shape} {str(dat_path)}")
         cyx_dat_record_list.append(cyx_dat_record)
 
-    compressed_data_list: list[np.ndarray] = compress_compute_layer(cyx_dat_record_list)
+    compressed_data_list: list[np.ndarray] = compress_compute_layer(cyx_dat_record_list,
+                                                                    channel_num=0,
+                                                                    fill_info=fill_info)
 
     for index in range(len(compressed_data_list)):
         compressed_path = compressed_path_list[index]
@@ -226,8 +211,15 @@ if __name__ == '__main__':
     # base_name = 'Merlin-6284_24-07-28_135631_0-0'
     dat_path_list = []
     compressed_path_list = []
-    for column in range(1):
+    for column in range(3):
         dat_path_list.append(Path(f'/Users/trautmane/Desktop/16_to_8/dat/{base_name}-{column}.dat'))
         compressed_path_list.append(Path(f'/Users/trautmane/Desktop/16_to_8/out_test/{base_name}-{column}.png'))
     compress_and_save_layer(dat_path_list=dat_path_list,
-                            compressed_path_list=compressed_path_list)
+                            compressed_path_list=compressed_path_list,
+                            fill_info=FillInfo(tile_indexes=[0],
+                                               x=2900,
+                                               y=470,
+                                               width=1650,
+                                               height=830,
+                                               fill_value=-1000,
+                                               threshold=0))
