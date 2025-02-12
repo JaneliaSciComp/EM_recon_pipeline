6ab567e63707debd3b0a928ca8d4c02c6241a0b7
Eric Trautman <trautmane@janelia.hhmi.org>
2025-02-12 16:52:04 -0500

    add fibsem and reiser labs to z_corr link script

--- src/scripts/cellmap/link_z_corr.sh
+++ src/scripts/cellmap/link_z_corr.sh
@@ -3,20 +3,21 @@
 #  To run every 10 minutes on renderer-data4 without trying to mail stdout/stderr:
 #
 #    crontab -e
-#      */10 * * * * /groups/flyem/data/render/git/EM_recon_pipeline/src/scripts/cellmap/link_z_corr.sh >/dev/null 2>&1
+#      */10 * * * * /groups/fibsem/home/fibsemxfer/git/EM_recon_pipeline/src/scripts/cellmap/link_z_corr.sh >/dev/null 2>&1
 #
 #  Crontab file is saved to /var/spool/cron/trautmane
 
 set -e
 
-cd /opt/local/jetty_base/webapps/z_corr_plots/cellmap
-
-for Z_CORR_BASE_DIR in /nrs/cellmap/data/jrc_*/z_corr/cellmap/jrc_*; do
-  NAME=$(basename "${Z_CORR_BASE_DIR}")
-  if [ -e "${NAME}" ]; then
-    echo "${NAME} already exists"
-  else
-    ln -s "${Z_CORR_BASE_DIR}" .
-    ls -ald "${NAME}"
-  fi
+for LAB in cellmap fibsem reiser; do
+  cd /opt/local/jetty_base/webapps/z_corr_plots/${LAB}
+  for Z_CORR_BASE_DIR in /nrs/"${LAB}"/data/*/z_corr/"${LAB}"/*; do
+    NAME=$(basename "${Z_CORR_BASE_DIR}")
+    if [ -e "${NAME}" ]; then
+      echo "${NAME} already exists"
+    else
+      ln -s "${Z_CORR_BASE_DIR}" .
+      ls -ald "${NAME}"
+    fi
+  done
 done
