1d3aeee7b50de50d7e70a13ceed98ca93d729fd7
Eric Trautman <trautmane@janelia.hhmi.org>
2025-09-05 20:50:19 -0400

    change magc slab to serial slab

--- src/scripts/msem_60_61/google_cloud/21_gc_upload.sh
+++ src/scripts/msem_60_61/google_cloud/21_gc_upload.sh
@@ -11,25 +11,14 @@ mkdir -p "${LOG_DIR}"
 LOG_FILE="${LOG_DIR}/gc_upload_${RUN_TIME}.log"
 
 if (( $# < 2 )); then
-  echo "USAGE $0 <wafer> <magc slab> [magc slab] ...     (e.g. 60 399)
-
-  wafer 60 serial slab 360 = magc slab 399
-  wafer 60 serial slab 361 = magc slab 298
-  wafer 60 serial slab 362 = magc slab 295
-  wafer 60 serial slab 363 = magc slab 70
-  wafer 60 serial slab 364 = magc slab 391
-  wafer 60 serial slab 365 = magc slab 410
-  wafer 60 serial slab 366 = magc slab 27
-  wafer 60 serial slab 367 = magc slab 381
-  wafer 60 serial slab 368 = magc slab 7
-  wafer 60 serial slab 369 = magc slab 380
+  echo "USAGE $0 <wafer> <serial slab> [serial slab] ...     (e.g. 60 360 361)
 "
   exit 1
 fi
 
 WAFER="$1"
 shift
-SLABS="$*"
+SERIAL_SLABS="$*"
 
 RENDER_OWNER="hess_wafers_60_61"
 NUM_THREADS=32
@@ -48,7 +37,7 @@ export PYTHONPATH="${EMRP_ROOT}/src/python"
 ARGS="${EMRP_ROOT}/src/python/janelia_emrp/msem/wafer_60_gc_upload/gc_upload.py"
 
 ARGS="${ARGS} --host http://em-services-1.int.janelia.org:8080/render-ws/v1"
-ARGS="${ARGS} --owner ${RENDER_OWNER} --wafer ${WAFER} --slabs ${SLABS}"
+ARGS="${ARGS} --owner ${RENDER_OWNER} --wafer ${WAFER} --slabs ${SERIAL_SLABS}"
 ARGS="${ARGS} --num-threads ${NUM_THREADS}"
 ARGS="${ARGS} --base-path  hess_wafer_60_data"
 ARGS="${ARGS} --invert"
