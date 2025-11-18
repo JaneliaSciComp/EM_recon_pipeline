17bb2a5a51c9caabd0f071ceb8c40d37c69b24b6
Eric Trautman <trautmane@janelia.hhmi.org>
2025-11-18 16:22:13 -0500

    add mask option for google cloud n5 exports and only append timestamp to dataset names when the original dataset exists

--- src/scripts/msem_60_61/google_cloud/11_run_n5_export.sh
+++ src/scripts/msem_60_61/google_cloud/11_run_n5_export.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if (( $# != 5 )); then
+if (( $# < 5 )); then
   echo "
-Usage:    ./11_run_n5_export.sh <render-ws-internal-ip> <render-project> <render-stack> <max-z> <max-executors>
+Usage:    ./11_run_n5_export.sh <render-ws-internal-ip> <render-project> <render-stack> <max-z> <max-executors> [mask]
 
           max-executors must be at least 2
 
@@ -17,26 +17,39 @@ RENDER_WS_IP="${1}"
 RENDER_PROJECT="${2}"
 STACK="${3}"
 MAX_Z="${4}"
-MAX_EXECUTORS="${5}"
-BATCH_ID_SUFFIX=$(echo "${STACK}" | tr '_' '-')
 
+MAX_EXECUTORS="${5}"
 if (( MAX_EXECUTORS < 2 )); then
   echo "ERROR: max-executors must be at least 2"
   exit 1
-fi
-
-if (( MAX_EXECUTORS > 500 )); then
+elif (( MAX_EXECUTORS > 500 )); then
   echo "ERROR: max-executors must be at most 500"
   exit 1
 fi
 
-SPARK_EXEC_CORES=4 # must be 4, 8, or 16
+DATASET_SUFFIX="${6:-pixel}"
+if [[ "${6}" == "mask" ]]; then
+  MASK_ARG="--exportMask"
+elif (( $# >= 6 )); then
+  echo "ERROR: sixth argument, if provided, must be 'mask'"
+  exit 1
+else
+  MASK_ARG=""
+fi
 
 RUN_TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+BATCH_ID_SUFFIX=$(echo "${STACK}" | tr '_' '-')
+SPARK_EXEC_CORES=4 # must be 4, 8, or 16
 
 RENDER_OWNER="hess_wafers_60_61"
-N5_PATH="gs://janelia-spark-test/hess_wafers_60_61_export"          # /nrs/hess/data/hess_wafers_60_61/export/hess_wafers_60_61.n5
-N5_DATASET="/render/${RENDER_PROJECT}/${STACK}___${RUN_TIMESTAMP}"  # /render/w60_serial_360_to_369/w60_s360_r00_d20_gc_align___20250320_131555
+N5_PATH="gs://janelia-spark-test/hess_wafers_60_61_export"           # /nrs/hess/data/hess_wafers_60_61/export/hess_wafers_60_61.n5
+N5_DATASET="/render/${RENDER_PROJECT}/${STACK}___${DATASET_SUFFIX}"  # /render/w60_serial_360_to_369/w60_s360_r00_d20_gc_align___20250320_131555
+
+if gcloud storage ls "${N5_PATH}${N5_DATASET}" | grep -q .; then
+  echo "
+Note: appending run time to dataset name since ${N5_PATH}${N5_DATASET} exists"
+  N5_DATASET="${N5_DATASET}_${RUN_TIMESTAMP}"
+fi
 
 # For standard compute tier and spark runtime, total of spark.memory.offHeap.size,
 # spark.executor.memory and spark.executor.memoryOverhead must be between 1024mb and 7424mb per core.
@@ -64,6 +77,7 @@ ARGS="${ARGS} --n5Dataset ${N5_DATASET}"
 ARGS="${ARGS} --tileWidth 2048 --tileHeight 2048 --blockSize 1024,1024,${MAX_Z} --factors 2,2,1"
 ARGS="${ARGS} --minZ 1 --maxZ ${MAX_Z}"
 # ARGS="${ARGS} --minX 84000 --maxX 94000 --minY 70000 --maxY 80000"
+ARGS="${ARGS} ${MASK_ARG}"
 
 echo "
 Running gcloud dataproc batches submit spark with:
