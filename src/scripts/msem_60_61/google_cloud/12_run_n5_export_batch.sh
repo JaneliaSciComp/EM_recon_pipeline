540a5cdfc508eb2957fdca288cc7e7ac3933ecd8
Eric Trautman <trautmane@janelia.hhmi.org>
2026-02-19 14:40:15 -0500

    minor improvements for 12_run_n5_export_batch.sh

--- src/scripts/msem_60_61/google_cloud/12_run_n5_export_batch.sh
+++ src/scripts/msem_60_61/google_cloud/12_run_n5_export_batch.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Utility script to simplify submission of multiple n5 export jobs.
+#
+# Copy this script locally (e.g. to export-batch.sh) so that STACK_NAMES_WITH_MAX_Z can be filled in
+# with details from running list-stacks.sh on VM
+# and MAX_EXECUTORS, IP, PIXEL_OR_MASK, and LAUNCH_JOBS values can be set.
+
 set -e
 
-NODES=20
-IP="10.150.0.2"
-LAUNCH_JOBS="n" # set to "y" to launch jobs, anything else to just print commands
+MAX_EXECUTORS=10       # 10 executors for w61_s099 pixel with 82 z layers took 4 hours, 15 minutes
+                       # 10 executors for w61_s122 mask  with 89 z layers took 1 hour,  30 minutes
+
+IP="10.150.0.4"        # c=10.150.0.4  d=10.150.0.2  e=10.150.0.3
+PIXEL_OR_MASK="pixel"  # mask
+LAUNCH_JOBS="n"        # set to "y" to launch jobs, anything else to just print commands
 
 # STACK_NAMES_WITH_MAX_Z examples:
 #   w61_s080_r00_gc_par_align_ic2d with maxZ 89
@@ -35,7 +44,7 @@ while read -r LINE; do
 
         PROJECT="${WAFER}_serial_${START}_to_${END}"
 
-        CMD="./11_run_n5_export.sh ${IP} ${PROJECT} ${STACK} ${MAX_Z} ${NODES}"
+        CMD="./11_run_n5_export.sh ${IP} ${PROJECT} ${STACK} ${MAX_Z} ${MAX_EXECUTORS} ${PIXEL_OR_MASK}"
 
         if [[ "${LAUNCH_JOBS}" == "y" ]]; then
           echo
