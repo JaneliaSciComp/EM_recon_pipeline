2ecde0580a536b9eac4d39791f550ea1c0803258
Eric Trautman <trautmane@janelia.hhmi.org>
2026-03-10 14:26:34 -0400

    fix 06_bg_correct_and_invert_images.sh

--- src/scripts/msem_68/06_bg_correct_and_invert_images.sh
+++ src/scripts/msem_68/06_bg_correct_and_invert_images.sh
@@ -2,20 +2,21 @@
 
 set -e
 
+umask 0002
+
 ABSOLUTE_SCRIPT=$(readlink -m "$0")
 SCRIPT_DIR=$(dirname "${ABSOLUTE_SCRIPT}")
 source "${SCRIPT_DIR}/00_config.sh"
 
-RUN_TIME=$(date +"%Y%m%d_%H%M%S")
-LOG_DIR="${SCRIPT_DIR}/logs"
-mkdir -p "${LOG_DIR}"
-LOG_FILE="${LOG_DIR}/bg_correct_${RUN_TIME}.log"
-
 RENDER_HOST="em-services-1.int.janelia.org"
 WAFER_ID="68"
 RENDER_OWNER="hess_sample_${WAFER_ID}_full"
-SOURCE_SUFFIX="_par_align_c"
+#SOURCE_SUFFIX="_par_align_c"
+OUTPUT_PATH="/nrs/hess/data/hess_sample_68_full/bg_corrected_inverted"
 NUM_THREADS=60
+
+mkdir -p "${OUTPUT_PATH}"
+chmod 2775 "${OUTPUT_PATH}"
 
 source /groups/hess/hesslab/render/bin/source_miniforge3.sh
 
@@ -31,10 +32,15 @@ export PYTHONPATH="${EMRP_ROOT}/src/python"
 ARGS="${EMRP_ROOT}/src/python/janelia_emrp/msem/background_correction/w_68_local.py"
 ARGS="${ARGS} --host ${RENDER_HOST} --owner ${RENDER_OWNER}"
 ARGS="${ARGS} --output-path ${OUTPUT_PATH}"
-ARGS="${ARGS} --suffix ${SOURCE_SUFFIX}"  # Stack name suffix to match (e.g. '_pa_mat_render_align'). If not given, matches bare region stacks like w68_s000_r00.
+#ARGS="${ARGS} --suffix ${SOURCE_SUFFIX}"  # Stack name suffix to match (e.g. '_pa_mat_render_align'). If not given, matches bare region stacks like w68_s000_r00.
 ARGS="${ARGS} --invert"                   # Invert the images after background correction.
 #ARGS="${ARGS} --shading-storage-path ..." # Storage path for shading (shading is not stored if path is not given).
 ARGS="${ARGS} --num-threads ${NUM_THREADS}"
+
+RUN_TIME=$(date +"%Y%m%d_%H%M%S")
+LOG_DIR="${SCRIPT_DIR}/logs"
+mkdir -p "${LOG_DIR}"
+LOG_FILE="${LOG_DIR}/bg_correct_${RUN_TIME}.log"
 
 echo """
 On ${HOSTNAME} at ${RUN_TIME}
