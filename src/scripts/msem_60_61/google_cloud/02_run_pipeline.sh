bcef467ccc25f608464a931874352b496ede09cd
Eric Trautman <trautmane@janelia.hhmi.org>
2026-08-22 20:58:35 -0400

    add executor disk size parameter to avoid Insufficient 'DISKS_TOTAL_GB' quota error from Google

--- src/scripts/msem_60_61/google_cloud/02_run_pipeline.sh
+++ src/scripts/msem_60_61/google_cloud/02_run_pipeline.sh
@@ -96,6 +96,7 @@ SPARK_PROPS="spark.dataproc.driver.compute.tier=${COMPUTE_TIER},spark.dataproc.e
 SPARK_PROPS="${SPARK_PROPS},spark.default.parallelism=240,spark.executor.instances=${SPARK_EXEC_INSTANCES}"
 SPARK_PROPS="${SPARK_PROPS},spark.executor.cores=${SPARK_EXEC_CORES},spark.executor.memory=${SPARK_EXEC_MEMORY_MB}mb"
 SPARK_PROPS="${SPARK_PROPS},${DYNAMIC_ALLOCATION}"
+SPARK_PROPS="${SPARK_PROPS},spark.dataproc.executor.disk.size=250g"
 #SPARK_PROPS="${SPARK_PROPS},spark.log.level.org.janelia.alignment.match=WARN"
 
 RUN_TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
