7b61bb83d46e83d338ace3a593a5b357d8995507
Eric Trautman <trautmane@janelia.hhmi.org>
2026-09-20 19:55:20 -0400

    update run time comment in load_data_for_02_align_run.sh

--- src/scripts/msem_60_61/google_cloud/load_data_for_02_align_run.sh
+++ src/scripts/msem_60_61/google_cloud/load_data_for_02_align_run.sh
@@ -150,7 +150,7 @@ On launch box, run:
 
 # 200 4-core executor run  took  20 minutes to complete for w61-s170-to-s174
 #  50 4-core executor runs take ~90 minutes to complete and 14 concurrent runs will use 2856 cores (204 cores per run)
-#  20 4-core executor runs take  ~4 hours   to complete and 26 concurrent runs will use 2184 cores ( 84 cores per run)
+#  20 4-core executor runs take  ~5 hours   to complete and 26 concurrent runs will use 2184 cores ( 84 cores per run)
 
 ./02_run_pipeline.sh  ${VM_IP}  ${STAGE}/pipe.02.w6n.align-stitch-only.json  20  4  premium  20  ${BATCH_NAME}  disableDynamic
 
